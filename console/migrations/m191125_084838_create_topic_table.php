1bdb0526e23f044a641accbae901971d7ce3b18b
arthmoon <36910865+arthmoon@users.noreply.github.com>
2019-11-27 12:43:31 +0300

    Create m191125_084838_create_topic_table.php

---
+++ console/migrations/m191125_084838_create_topic_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%topic}}`.
+ */
+class m191125_084850_create_post_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%post}}', [
+            'id' => $this->primaryKey(),
+            'post_id' => $this->string(250),
+            'author_href' => $this->string(250),
+            'author_thumb' => $this->string(250),
+            'author_name' => $this->string(250),
+            'datetime' => $this->dateTime(),
+            'content' => $this->text(),
+            'phone' => $this->string(250)
+        ], $tableOptions);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%post}}');
+    }
+}
